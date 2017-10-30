c42dbd7ebe28ec4936264ba6a2ad24ff9375adef
ibmcloudtools <ibmcloudtools@us.ibm.com>
2017-10-30 15:49:40 -0500

    asd

---
+++ phase_deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+mkdir -p ~/.ssh
+echo $SSH_PRIVATE_KEY >> ~/.ssh/ssh_private_key_tmp
+echo -----BEGIN RSA PRIVATE KEY----- >> ~/.ssh/ssh_private_key
+sed 's/\s\+/\n/g' ~/.ssh/ssh_private_key_tmp >> ~/.ssh/ssh_private_key
+echo -----END RSA PRIVATE KEY----- >> ~/.ssh/ssh_private_key
+cat ~/.ssh/ssh_private_key
+chmod 600 ~/.ssh/ssh_private_key
+touch ~/.ssh/known_hosts
+ssh-keygen -f ~/.ssh/known_hosts -R $HOST_ADDRESS
+ssh-keyscan $HOST_ADDRESS >> ~/.ssh/known_hosts 
+ssh -i ~/.ssh/ssh_private_key root@$HOST_ADDRESS mkdir -p app
+rsync -arv -e "ssh -i $HOME/.ssh/ssh_private_key" . root@$HOST_ADDRESS:app
+ssh -i ~/.ssh/ssh_private_key root@$HOST_ADDRESS "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
---
+++ phase_schematics.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+bx plugin install schematics
+bx api https://api.ng.bluemix.net
+bx login --apikey $BLUEMIX_API_KEY -c $BLUEMIX_ACCOUNT_ID
+bx target -o $BLUEMIX_ORG -s $BLUEMIX_SPACE
+bx schematics environment show -id $SCHEMATICS_ENV_ID
+
+COUNTER=0
+echo Running IBM Schematics, will wait for up to 20 minutes
+act_id=$(bx schematics action apply --id $SCHEMATICS_ENV_ID --force | grep activity | awk '{print $2}')
+if [ "${#act_id}" -ne 32 ]; then
+    echo Invalid activity_id
+    exit 1
+fi
+echo IBM Schemativcs activity_id $act_id
+
+# 120 intervals of 10 seconds == 20 minutes
+while [ $COUNTER -lt 120 ]; do
+    act_status=$(bx schematics activity show --id $act_id | grep Status | awk '{print $2}')
+    echo IBM Schematics activity_status $act_status
+    if [ "$act_status" == "INPROGRESS" ] || [ "$act_status" == "CREATED" ]; then
+        echo Activity is still in progress.... Waiting....
+        let COUNTER=COUNTER+1 
+        sleep 10
+    elif [ "$act_status" == "COMPLETED" ]; then
+        echo Activity successfully completed 
+        exit 0
+    else 
+        echo Unknown activity status, see below for more details
+        bx schematics activity show --id $act_id
+        exit -1
+    fi
+done
