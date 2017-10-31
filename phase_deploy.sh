505731e48fb549987ae101dda53fdefcc6479764
ibmcloudtools <ibmcloudtools@us.ibm.com>
2017-10-30 20:09:39 -0500

    a

--- phase_deploy.sh
+++ phase_deploy.sh
@@ -7,5 +7,5 @@ touch ~/.ssh/known_hosts
 ssh-keygen -f ~/.ssh/known_hosts -R $HOST_ADDRESS
 ssh-keyscan $HOST_ADDRESS >> ~/.ssh/known_hosts 
 ssh -i ssh_private_key root@$HOST_ADDRESS mkdir -p app
-rsync -arv -e "ssh -i $HOME/ssh_private_key" . root@$HOST_ADDRESS:app
+rsync -arv -e "ssh -i ssh_private_key" . root@$HOST_ADDRESS:app
 ssh -i ssh_private_key root@$HOST_ADDRESS "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
