37ad18ae15c9e15d0e954191c56d1538231e7939
ibmcloudtools <ibmcloudtools@us.ibm.com>
2017-10-31 12:24:43 -0500

    asdf

--- phase_deploy.sh
+++ phase_deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -x
-HOST_ADDRESS=$(cat vsi_ip.txt)
-touch ~/.ssh/known_hosts
-ssh-keygen -f ~/.ssh/known_hosts -R $HOST_ADDRESS
-ssh-keyscan $HOST_ADDRESS >> ~/.ssh/known_hosts 
-ssh -i ssh_private_key root@$HOST_ADDRESS mkdir -p app
-rsync -arv -e "ssh -i ssh_private_key" . root@$HOST_ADDRESS:app
-ssh -i ssh_private_key root@$HOST_ADDRESS "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
+VSI_HOSTS=$(cat vsi_ip.txt)
+for VSI_HOST in $VSI_HOSTS
+do
+    echo $VSI_HOST
+    touch ~/.ssh/known_hosts
+    ssh-keygen -f ~/.ssh/known_hosts -R $VSI_HOST
+    ssh-keyscan $VSI_HOST >> ~/.ssh/known_hosts 
+    ssh -i ssh_private_key root@$VSI_HOST mkdir -p app
+    rsync -arv -e "ssh -i ssh_private_key" . root@$VSI_HOST:app
+    ssh -i ssh_private_key root@$VSI_HOST "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
+done
