d03e2b9f3542305dd483d618d5c1545ba59186c2
ibmcloudtools <ibmcloudtools@us.ibm.com>
2017-10-30 19:55:12 -0500

    asdf

--- phase_deploy.sh
+++ phase_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+ls -la
 #mkdir -p ~/.ssh
 #echo $SSH_PRIVATE_KEY >> ~/.ssh/ssh_private_key_tmp
 #echo -----BEGIN RSA PRIVATE KEY----- >> ~/.ssh/ssh_private_key
@@ -9,6 +10,6 @@ HOST_ADDRESS=$(cat vsi_ip.txt)
 touch ~/.ssh/known_hosts
 ssh-keygen -f ~/.ssh/known_hosts -R $HOST_ADDRESS
 ssh-keyscan $HOST_ADDRESS >> ~/.ssh/known_hosts 
-ssh -i ~/.ssh/ssh_private_key root@$HOST_ADDRESS mkdir -p app
-rsync -arv -e "ssh -i $HOME/.ssh/ssh_private_key" . root@$HOST_ADDRESS:app
-ssh -i ~/.ssh/ssh_private_key root@$HOST_ADDRESS "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
+ssh -i ssh_private_key root@$HOST_ADDRESS mkdir -p app
+rsync -arv -e "ssh -i $HOME/ssh_private_key" . root@$HOST_ADDRESS:app
+ssh -i ssh_private_key root@$HOST_ADDRESS "pkill node; cd app; npm install; nohup npm start > /dev/null 2>&1 &"
--- phase_schematics.sh
+++ phase_schematics.sh
@@ -33,14 +33,13 @@ while [ $COUNTER -lt 120 ]; do
     fi
 done
 
-# Save IP address to a text file
+# Save VSI IP address to a text file
 bx schematics activity log --id $act_id | grep vm_instance_ipv4_address | grep "Terraform show" | awk '{print $8}' | head -1 > vsi_ip.txt
 
 # Save private key to a text file
-mkdir -p ~/.ssh
-echo -----BEGIN RSA PRIVATE KEY----- >> ~/.ssh/ssh_private_key
-bx schematics activity log --id $act_id | grep "Terraform apply" | grep MIIE -A 24 | awk -F'|' '{print $2}' | awk '{print $1}' >> ~/.ssh/ssh_private_key
-echo -----END RSA PRIVATE KEY----- >> ~/.ssh/ssh_private_key
-chmod 600 ~/.ssh/ssh_private_key
-cat ~/.ssh/ssh_private_key
+echo -----BEGIN RSA PRIVATE KEY----- >> ssh_private_key
+bx schematics activity log --id $act_id | grep "Terraform apply" | grep MIIE -A 24 | awk -F'|' '{print $2}' | awk '{print $1}' >> ssh_private_key
+echo -----END RSA PRIVATE KEY----- >> ssh_private_key
+chmod 600 ssh_private_key
+cat ssh_private_key
 
